2ffe9edd0a4da8c82f2c003f01c1183ad02c81bb
Кузьминых Сергей <kuzminyhsa@gmail.com>
2024-10-22 07:51:52 +0800

    first commit

---
+++ src/main/java/kata/example/kata_spring_boot/KataSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kata.example.kata_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/example/kata_spring_boot/KataSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.example.kata_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
